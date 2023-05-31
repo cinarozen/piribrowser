f9d15971f8aa15c6b47774164e5109b6bb1e1dce
cinarozen <134882157+cinarozen@users.noreply.github.com>
2023-05-31 21:35:17 +0300

    Update browser.py

--- browser.py
+++ browser.py
@@ -3,29 +3,56 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
+class HistoryWindow(QDialog):
+    def __init__(self, history):
+        super().__init__()
+        self.setWindowTitle("Geçmiş")
+        self.resize(512,384)
+
+        self.history_list = QListView()
+        self.history_list.setModel(history)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.history_list)
+        self.setLayout(layout)
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Piri Browser v1 source codes")
-        self.resize(1024, 768)
+        self.resize(1280, 720)
+
         self.search_bar = QLineEdit()
         self.search_bar.returnPressed.connect(self.search_keyword)
+
         self.browser = QWebEngineView()
         self.browser.load(QUrl("http://piribrowser.rf.gd/"))
+
         self.back_button = QPushButton("<")
         self.back_button.clicked.connect(self.browser.back)
+
         self.forward_button = QPushButton(">")
         self.forward_button.clicked.connect(self.browser.forward)
+
         self.reload_button = QPushButton("Yenile")
         self.reload_button.clicked.connect(self.browser.reload)
+
         self.main_menu_button = QPushButton("Ana sayfa")
+        self.main_menu_button.clicked.connect(self.go_home)
+
         self.eba = QPushButton("Eba")
         self.eba.clicked.connect(self.ebago)
-        self.main_menu_button.clicked.connect(self.go_home)
-        self.eokul=QPushButton("E-Okul")
+
+        self.eokul = QPushButton("E-Okul")
         self.eokul.clicked.connect(self.goeokul)
+
+        self.history = QStandardItemModel()
+        self.history_button = QPushButton("Geçmişi Göster")
+        self.history_button.clicked.connect(self.show_history)
+
+
         layout = QVBoxLayout()
         layout.addWidget(self.search_bar)
+
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.back_button)
         button_layout.addWidget(self.forward_button)
@@ -33,26 +60,34 @@ class MainWindow(QMainWindow):
         button_layout.addWidget(self.main_menu_button)
         button_layout.addWidget(self.eba)
         button_layout.addWidget(self.eokul)
+        button_layout.addWidget(self.history_button)
+
         layout.addLayout(button_layout)
         layout.addWidget(self.browser)
+
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
+
+        self.browser.urlChanged.connect(self.update_history)
+
     def search_keyword(self):
-        search_query = self.search_bar.text().replace(" ", "+")
-        if "http" in self.search_bar.text() or "www" in self.search_bar.text():
-            url = self.search_bar.text()
-        else:
-            url = f"https://cse.google.com/cse?cx=845a07a7a23a248a5#gsc.tab=0&gsc.q={search_query}"
+        url = self.search_bar.text()
         self.browser.load(QUrl(url))
-    def anasayfa(self):
-        self.browserload(QUrl("http://piribrowser.rf.gd/"))
-    def ebago(self):
-        self.browser.load(QUrl("https://www.eba.gov.tr"))
+
+    def update_history(self, url):
+        item = QStandardItem(url.toString())
+        self.history.appendRow(item)
+
+    def show_history(self):
+        dialog = HistoryWindow(self.history)
+        dialog.exec_()
     def go_home(self):
-        self.browser.load(QUrl("http://piribrowser.rf.gd/"))
+       self.browser.load(QUrl("http://piribrowser.rf.gd/"))
+    def ebago(self):
+        self.browser.load(QUrl("http://eba.gov.tr/"))
     def goeokul(self):
-        self.browser.load(QUrl("https://e-okul.meb.gov.tr"))
+        self.browser.load(QUrl("https://eokulyd.meb.gov.tr/"))
 app = QApplication(sys.argv)
 mainwindow = MainWindow()
 mainwindow.show()
